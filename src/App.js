0ced32e94f88047931f5082a68c6887110600961
Jy0042 <sjy6630@gmail.com>
2023-07-25 03:15:31 +0900

    React useState 데이터 수정(deepcopy) 공부 및 ES6 신문법 공부

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-
+  
   let [글제목,글제목변경] = useState(['리액트 공부', '재밌어요', '블로그 빨리 만들어야하는데..']);
+  let [like, likeIncrease] = useState(0);
   let posts = '블로그 만들기 시작';
 
+  function changeTitle() {
+    var newArray = [...글제목];
+    newArray[0] = 'React Study';
+    글제목변경(newArray);
+  }
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
+      <button onClick={ changeTitle }>버튼</button>
       <div className="list">
-        <h3> { 글제목[0] } </h3>
+        <h3> { 글제목[0] } <span onClick={ () => { likeIncrease(like + 1) } }>❤️</span> { like } </h3>
         <p>23년 7월 25일</p>
         <hr />
       </div>
