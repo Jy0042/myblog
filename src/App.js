6b9269489e53a2192239bb59b4db3a94fae2dc4c
Jy0042 <sjy6630@gmail.com>
2023-07-25 01:54:08 +0900

    React로 블로그 만들기 JSX 와 useState 공부

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Post from './pages/Post';
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
+
+  let [글제목,글제목변경] = useState(['리액트 공부', '리액트 재밌어요']);
+
+  let posts = '블로그 만들기 시작';
+
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/post/:id" element={<Post />} />
-        </Routes>
-      </Router>
+    <div className="App">
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <div className="list">
+        <h3> { 글제목[0] } </h3>
+        <p>23년 7월 25일</p>
+        <hr />
+      </div>
     </div>
   );
 }
