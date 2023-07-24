002ba20e5731fdb52e2d0a710f18f6435c1b5d3f
Jy0042 <sjy6630@gmail.com>
2023-07-25 04:05:38 +0900

    React Component 공부 -  모달로 상세페이지 구현

--- src/App.js
+++ src/App.js
@@ -7,17 +7,24 @@ function App() {
   let [like, likeIncrease] = useState(0);
   let posts = '블로그 만들기 시작';
 
-  function changeTitle() {
-    var newArray = [...글제목];
-    newArray[0] = 'React Study';
-    글제목변경(newArray);
-  }
+  // function changeTitle() {
+  //   var newArray = [...글제목];
+  //   newArray[0] = 'React Study';
+  //   글제목변경(newArray);
+  // }
+
+  // function arrange() {
+  //   var newArray = [...글제목];
+  //   newArray.sort();
+  //   글제목변경(newArray);
+  // }
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <button onClick={ changeTitle }>버튼</button>
+      {/* <button onClick={ changeTitle }>버튼</button>
+      <button onClick={ arrange }>버튼</button> */}
       <div className="list">
         <h3> { 글제목[0] } <span onClick={ () => { likeIncrease(like + 1) } }>❤️</span> { like } </h3>
         <p>23년 7월 25일</p>
@@ -33,8 +40,21 @@ function App() {
         <p>23년 7월 27일</p>
         <hr />
       </div>
+      
+      <Modal></Modal>
+
     </div>
   );
+}
+
+function Modal () {
+  return (
+    <div className='modal'>
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세내용</p>        
+    </div>
+  )
 }
 
 export default App;
