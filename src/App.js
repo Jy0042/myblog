6946d43db24a6b1b8edee718ecab0b72e70a91e2
Jy0042 <sjy6630@gmail.com>
2023-07-25 02:04:38 +0900

    강의 숙제 useState 사용해서 리스트 3개 출력하기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  let [글제목,글제목변경] = useState(['리액트 공부', '리액트 재밌어요']);
-
+  let [글제목,글제목변경] = useState(['리액트 공부', '재밌어요', '블로그 빨리 만들어야하는데..']);
   let posts = '블로그 만들기 시작';
 
   return (
@@ -16,6 +14,16 @@ function App() {
       <div className="list">
         <h3> { 글제목[0] } </h3>
         <p>23년 7월 25일</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3> { 글제목[1] } </h3>
+        <p>23년 7월 26일</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3> { 글제목[2] } </h3>
+        <p>23년 7월 27일</p>
         <hr />
       </div>
     </div>
