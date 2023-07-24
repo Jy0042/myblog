140eae9d83592caf2d601df5430bc40ed440dfa0
Jy0042 <sjy6630@gmail.com>
2023-07-25 00:02:45 +0900

    블로그 만들기 시작

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   const posts = [
     { id: 1, title: '첫 번째 포스트', content: '포스트 1의 내용입니다.' },
-    { id: 2, title: '두 번째 포스트', content: '포스트 2의 내용입니다.'  }];
+    { id: 2, title: '두 번째 포스트', content: '포스트 2의 내용입니다.'  }
+  ];
 
   return (
     <div>
